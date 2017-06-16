e10955213043b3181605ec3f768dfc1de844c432
Alexey Sviridov <sviridov.alexey@gmail.com>
2017-06-16 12:57:07 +0700

    Разрешать изменение данных чека при повторную отправке callbackUrl

--- install/tools/cashboxmodul_print_callback.php
+++ install/tools/cashboxmodul_print_callback.php
@@ -5,24 +5,23 @@ use Bitrix\Main;
 use Bitrix\Sale\Cashbox\Internals\CashboxCheckTable;
 use Modulpos\Cashbox\CashboxModul;
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 
 if (!CModule::IncludeModule("sale")) {
-	return;
+    return;
 }
 
 defined('MODULE_NAME') or define('MODULE_NAME', 'modulpos.cashbox');
 
 if (!CModule::IncludeModule(MODULE_NAME))
-	return;
+    return;
 
 $request = Main\Application::getInstance()->getContext()->getRequest();
 
-if (!CashboxModul::validateToken($request->get('token'), $request->get('docId')))
-{
-	CHTTP::SetStatus("403 Forbidden");
-	$APPLICATION->FinalActions();
-	die();
+if (!CashboxModul::validateToken($request->get('token'), $request->get('docId'))) {
+    CHTTP::SetStatus("403 Forbidden");
+    $APPLICATION->FinalActions();
+    die();
 }
 
 $printedDocId = $request->get('docId');
@@ -31,22 +30,17 @@ $qrCode = urldecode($request->get('qr'));
 CashboxModul::log("Callback called, docId=$printedDocId qr=$qrCode");
 
 
-
 $dbRes = CashboxCheckTable::getList(array('select' => array('STATUS'), 'filter' => array('ID' => $printedDocId)));
 $data = $dbRes->fetch();
-if ($data)
-{
-    if ($data['STATUS'] !== 'Y')
-    {
-        $result = CashboxCheckTable::update(
-            $printedDocId,
-            array(
-                'STATUS' => 'Y',
-                'LINK_PARAMS' => array('qr' => $qrCode),
-                'DATE_PRINT_END' => new Main\Type\DateTime()
-            )
-        );
-    }
+if ($data) {
+    $result = CashboxCheckTable::update(
+        $printedDocId,
+        array(
+            'STATUS' => 'Y',
+            'LINK_PARAMS' => array('qr' => $qrCode),
+            'DATE_PRINT_END' => new Main\Type\DateTime()
+        )
+    );
 }
 
 print "Completed";
