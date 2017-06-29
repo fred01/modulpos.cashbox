1cd035b406cf25d9d3fca7fcddaff5077b5d17e7
Alexey Sviridov <sviridov.alexey@gmail.com>
2017-06-29 16:40:18 +0700

    незначительно: удаленны комментарии

--- options.php
+++ options.php
@@ -147,14 +147,6 @@ $tabControl->begin();
         <td width="60%">
             <? $defaultVatTag = Option::get(MODULE_CASHBOX_NAME, 'default_vat_tag', '1105') ?>
             <select name="default_vat_tag">
-
-                //        1104 - "НДС 0%",
-                //        1103 - "НДС 10%",
-                //        1102 - "НДС 18%",
-                //        1105 - "НДС не облагается",
-                //        1107 - "НДС с рассч. ставкой 10%",
-                //        1106 - "НДС с рассч. ставкой 18%",
-
                 <option value="1102" <?=($defaultVatTag == '1102'?'selected':'')?> ><?=Loc::getMessage("REFERENCES_VAT_18")?></option>
                 <option value="1103" <?=($defaultVatTag == '1103'?'selected':'')?> ><?=Loc::getMessage("REFERENCES_VAT_10")?></option>
                 <option value="1104" <?=($defaultVatTag == '1104'?'selected':'')?> ><?=Loc::getMessage("REFERENCES_VAT_0")?></option>
